e1111b6d9c4baa6bfa5ab9b9215e8b049f6a3f8a
Illia <61018909+Ilmovchan@users.noreply.github.com>
2023-12-24 22:39:42 +0200

    lab 4

---
+++ lab #4/StackInterface/IStack.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace StackInterface
+{
+	public interface IStack<T>
+	{
+		void Push(T val);
+        void Clear();
+		T Pop();
+		T Peek();
+	}
+
+    public class MyStack<T> : IStack<T>
+    {
+
+        public List<T> elements;
+        public int Count { get; private set; }
+        public T Top { get; private set; }
+
+        public MyStack()
+        {
+            elements = new List<T>();
+            Count = 0;
+            Top = default(T);
+        }
+
+        public void Push(T val)
+        {
+            elements.Add(val);
+            Count++;
+            Top = elements[elements.Count - 1];
+        }
+
+        public T Pop()
+        {
+            if (elements.Count == 0) return default(T);
+
+            T lastElement = elements[elements.Count - 1];
+            elements.RemoveAt(elements.Count - 1);
+            Count--;
+            Top = elements[elements.Count - 1];
+            return lastElement;
+        }
+
+        public T Peek()
+        {
+            if (elements.Count == 0) return default(T);
+
+            T lastElement = elements[elements.Count - 1];
+            return lastElement;
+        }
+
+        public void Clear()
+        {
+            elements.Clear();
+            Top = default(T);
+            Count = 0;
+        }
+
+    }
+}
+
--- lab #4/StackInterface/Program.cs
+++ lab #4/StackInterface/Program.cs
@@ -4,6 +4,18 @@
     {
         static void Main(string[] args)
         {
+            MyStack<int> stack = new MyStack<int>();
+            stack.Push(5);
+            stack.Push(16);
+            stack.Push(-3);
+
+            stack.Push(31);
+            stack.Push(1);
+            stack.Push(-1);
+
+            stack.Clear();
+
+            foreach (var i in stack.elements) Console.WriteLine(i);
         }
     }
 }
