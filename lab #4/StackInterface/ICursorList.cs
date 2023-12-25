90accab8b79ef32918c2f947ec90e8477a0d2987
Illia <61018909+Ilmovchan@users.noreply.github.com>
2023-12-25 13:59:34 +0200

    queue and cursor list

---
+++ lab #4/StackInterface/ICursorList.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace StackInterface
+{
+	public interface ICursorList<T>
+	{
+		void Next();
+		void Previous();
+		T getCurrent();
+		void RemoveCurrent();
+		void Clear();
+		void Add(T val);
+	}
+
+    public class CursorList<T> : ICursorList<T>
+    {
+        List<T> elements;
+        public int Cursor { get; private set; }
+        public int Count { get; private set; }
+
+        CursorList()
+        {
+            elements = new List<T>();
+            Cursor = 0;
+            Count = 0;
+        }
+
+        public void Add(T val)
+        {
+            elements.Add(val);
+            Count++;
+        }
+
+        public void Clear()
+        {
+            elements.Clear();
+            Count = 0;
+        }
+
+        public T getCurrent()
+        {
+            if (Cursor >= 0 && Cursor <= elements.Count - 1)
+            {
+                return elements[Cursor];
+            }
+            return default(T);
+        }
+
+        public void Next()
+        {
+            if (Cursor != elements.Count - 1) Cursor++;
+        }
+
+        public void Previous()
+        {
+            if (Cursor != 0) Cursor--;
+        }
+
+        public void RemoveCurrent()
+        {
+            if (Cursor >= 0 && Cursor <= elements.Count - 1)
+            {
+                elements.RemoveAt(Cursor);
+                Count--;
+            }
+        }
+    }
+}
+
---
+++ lab #4/StackInterface/IQueue.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace StackInterface
+{
+    public interface IQueue<T>
+    {
+        void Enqueue(T val);
+        T Dequeue();
+        T Peek();
+        void Clear();
+    }
+
+    public class SimpleQueue<T> : IQueue<T>
+    {
+        List<T> items;
+        public int Count { get; private set; }
+
+        public SimpleQueue()
+        {
+            items = new List<T>();
+            Count = 0;
+        }
+
+        public void Enqueue(T val)
+        {
+            items.Add(val);
+            Count++;
+        }
+
+        public T Dequeue()
+        {
+            if (items.Count == 0) return default(T);
+
+            T val = items[0];
+            items.RemoveAt(0);
+            Count--;
+            return val;
+        }
+
+        public T Peek()
+        {
+            if (items.Count == 0) return default(T);
+            return items[0];
+        }
+
+        public void Clear()
+        {
+            items.Clear();
+            Count = 0;
+        }
+    }
+}
+
